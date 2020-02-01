d2f41a8ba5023afee158d3fb222d7c291c5bc4f9
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-01 17:39:05 +0900

    add files

---
+++ flow.go
@@ -0,0 +1,21 @@
+package dii
+
+// JoinFlow is a struct for defining injection connection flow.
+type JoinFlow struct {
+}
+
+func (f *JoinFlow) NewContainer() {
+
+}
+
+func (f *JoinFlow) To() {
+
+}
+
+func (f *JoinFlow) Join() {
+
+}
+
+func (f *JoinFlow) Drop() {
+
+}
---
+++ version.go
@@ -0,0 +1,3 @@
+package dii
+
+const Version = "0.0.1"
